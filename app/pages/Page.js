90f9dc5d8e577caa1857b1ecc496b4d7372f0cd2
邱国辉 <530784837@qq.com>
2017-01-22 18:20:29 +0800

    正在玩首页

--- app/pages/Page.js
+++ app/pages/Page.js
@@ -11,10 +11,10 @@ import {
   Navigator,
   TouchableOpacity,
   StatusBar,
-} from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+} from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
-import routesConfig from '../route-config';
+import routesConfig from '../route-config'
 
 
 export default class NavigationBar extends Component {
@@ -27,11 +27,6 @@ export default class NavigationBar extends Component {
       rightBtnName: '',
       title: '',
     }
-
-    // 读取props的属性
-    for (let attr in props) {
-      this.state[attr] = props[attr]
-    }
   }
 
   // 监听右侧按钮
@@ -42,21 +37,26 @@ export default class NavigationBar extends Component {
   // 组件内部定义的事件
   onRightButtonCallBack () {}
 
+  // ref回调函数
   refCallBack = (ref) => {
-    this.state.title = ref.state.title || this.props.initTitle
-    this.state.showBackBtn = ref.state.showBackBtn || false
-    this.showRightBtn = Boolean(ref.state.rightBtnName)
-    this.state.rightBtnName = ref.state.rightBtnName
+    if (!ref) return
+    this.setState({
+      title: ref.state.title || this.props.initTitle,
+      showBackBtn: ref.state.showBackBtn || false,
+      showRightBtn: Boolean(ref.state.rightBtnName),
+      rightBtnName: ref.state.rightBtnName,
+    })
     this.onRightButtonCallBack = ref.onRightButton
   }
 
   // 渲染页面
   renderScene = (route,navigator) => {
     var Component = this.props.initComponent || route.component
-    return <Component
-      ref={(ref) => {this.onRightButtonCallBack = ref && ref.onRightButton}}
+    var result = <Component
+      ref={this.refCallBack}
       {...route}
       navigator={navigator} />
+    return result
   }
 
   // 头部导航栏
@@ -81,7 +81,7 @@ export default class NavigationBar extends Component {
           this.state.showRightBtn ?
           <TouchableOpacity style={styles.navBarMenu} onPress={this.onRightButton}>
             <Text style={styles.menuText}>
-              <Icon name="navicon" style={styles.menuText}/>
+              <Icon name={this.state.rightBtnName} style={styles.menuText}/>
             </Text>
           </TouchableOpacity>
           : null
@@ -101,7 +101,7 @@ export default class NavigationBar extends Component {
 
   render() {
     return (
-      <Navigator
+      <Navigator style={styles.page}
         initialRoute={{component: this.props.initComponent}}
         configureScene={routesConfig.configureScene}
         renderScene={this.renderScene}
@@ -116,51 +116,13 @@ export default class NavigationBar extends Component {
   }
 }
 
-var NavigationBarRouteMapper = function () {
-  return {
-    LeftButton: function(route, navigator, index, navState) {
-      return (
-        <TouchableOpacity
-          style={styles.navBarBack}
-          onPress={() => navigator.pop()}>
-          <Text style={[styles.BackText]}>
-            <Icon name="angle-left" style={[styles.BackText, {fontSize: 19}]} />
-            返回
-          </Text>
-        </TouchableOpacity>
-      );
-    },
-    RightButton: function(route, navigator, index, navState) {
-      return (
-        <TouchableOpacity style={styles.navBarMenu} onPress={() => {
-          this.onRightButton()
-        }}>
-          <Text style={styles.menuText}>
-            <Icon name="navicon" style={styles.menuText}/>
-          </Text>
-        </TouchableOpacity>
-      );
-    },
-
-    Title: function(route, navigator, index, navState) {
-      return (
-        <View>
-          <Text style={[styles.navBarTitle]}>{route.title}</Text>
-          <StatusBar barStyle="light-content" />
-        </View>
-      );
-    },
-  };
-};
-
 const styles = StyleSheet.create({
+  page: {
+    paddingTop: 60,
+  },
   navBarBox: {
     height: 60,
-    paddingTop: 20,
     backgroundColor: '#333238',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'flex-end'
   },
   navBarTitle: {
     height: 40,
@@ -171,8 +133,6 @@ const styles = StyleSheet.create({
   },
   navBarMenu: {
     paddingRight: 10,
-    height: 40,
-    overflow: 'hidden',
   },
   BackText: {
     lineHeight: 45,
--- app/pages/Wechat.js
+++ app/pages/Wechat.js
@@ -4,27 +4,220 @@ import {
     Navigator,
     TouchableOpacity,
     Text,
+    Image,
+    StyleSheet,
+    ScrollView,
 } from 'react-native';
 
 import Header from '../components/Header'
 
 export default class Wechat extends Component {
     constructor(props) {
-        super(props);
-        this.state = {};
+      super(props);
+      this.state = {
+        title: '微信',
+        rightBtnName: 'plus',
+        messages: [
+          {
+            image: 'https://user-gold-cdn.xitu.io/2016/11/29/ebfdcade79bfb709f9a295fc70a39e42',
+            name: '名字',
+            content: '聊天内容聊天内容聊天内容聊天内容聊天内容聊天内容聊天内容聊天内容',
+            time: '下午6:15'
+          },
+          {
+            image: 'https://dn-mhke0kuv.qbox.me/6fe34414c019596859e1.jpg?imageView2/1/w/164/h/164/q/85/format/jpg/interlace/1',
+            name: '名字2',
+            content: '聊天内容聊天内容',
+            time: '下午6:15'
+          },
+          {
+            image: 'https://dn-mhke0kuv.qbox.me/6fe34414c019596859e1.jpg?imageView2/1/w/164/h/164/q/85/format/jpg/interlace/1',
+            name: '名字3',
+            content: '聊天内容聊天内容',
+            time: '下午6:15'
+          },
+          {
+            image: 'https://dn-mhke0kuv.qbox.me/6fe34414c019596859e1.jpg?imageView2/1/w/164/h/164/q/85/format/jpg/interlace/1',
+            name: '名字3',
+            content: '聊天内容聊天内容',
+            time: '下午6:15'
+          },
+          {
+            image: 'https://dn-mhke0kuv.qbox.me/6fe34414c019596859e1.jpg?imageView2/1/w/164/h/164/q/85/format/jpg/interlace/1',
+            name: '名字3',
+            content: '聊天内容聊天内容',
+            time: '下午6:15'
+          },
+          {
+            image: 'https://dn-mhke0kuv.qbox.me/6fe34414c019596859e1.jpg?imageView2/1/w/164/h/164/q/85/format/jpg/interlace/1',
+            name: '名字3',
+            content: '聊天内容聊天内容',
+            time: '下午6:15'
+          },
+          {
+            image: 'https://dn-mhke0kuv.qbox.me/6fe34414c019596859e1.jpg?imageView2/1/w/164/h/164/q/85/format/jpg/interlace/1',
+            name: '名字3',
+            content: '聊天内容聊天内容',
+            time: '下午6:15'
+          },
+          {
+            image: 'https://dn-mhke0kuv.qbox.me/6fe34414c019596859e1.jpg?imageView2/1/w/164/h/164/q/85/format/jpg/interlace/1',
+            name: '名字3',
+            content: '聊天内容聊天内容',
+            time: '下午6:15'
+          },
+          {
+            image: 'https://dn-mhke0kuv.qbox.me/6fe34414c019596859e1.jpg?imageView2/1/w/164/h/164/q/85/format/jpg/interlace/1',
+            name: '名字3',
+            content: '聊天内容聊天内容',
+            time: '下午6:15'
+          },
+          {
+            image: 'https://dn-mhke0kuv.qbox.me/6fe34414c019596859e1.jpg?imageView2/1/w/164/h/164/q/85/format/jpg/interlace/1',
+            name: '名字3',
+            content: '聊天内容聊天内容',
+            time: '下午6:15'
+          },
+          {
+            image: 'https://dn-mhke0kuv.qbox.me/6fe34414c019596859e1.jpg?imageView2/1/w/164/h/164/q/85/format/jpg/interlace/1',
+            name: '名字3',
+            content: '聊天内容聊天内容',
+            time: '下午6:15'
+          },
+          {
+            image: 'https://dn-mhke0kuv.qbox.me/6fe34414c019596859e1.jpg?imageView2/1/w/164/h/164/q/85/format/jpg/interlace/1',
+            name: '名字3',
+            content: '聊天内容聊天内容',
+            time: '下午6:15'
+          },
+          {
+            image: 'https://dn-mhke0kuv.qbox.me/6fe34414c019596859e1.jpg?imageView2/1/w/164/h/164/q/85/format/jpg/interlace/1',
+            name: '名字3',
+            content: '聊天内容聊天内容',
+            time: '下午6:15'
+          },
+          {
+            image: 'https://dn-mhke0kuv.qbox.me/6fe34414c019596859e1.jpg?imageView2/1/w/164/h/164/q/85/format/jpg/interlace/1',
+            name: '名字3',
+            content: '聊天内容聊天内容',
+            time: '下午6:15'
+          },
+          {
+            image: 'https://dn-mhke0kuv.qbox.me/6fe34414c019596859e1.jpg?imageView2/1/w/164/h/164/q/85/format/jpg/interlace/1',
+            name: '名字3',
+            content: '聊天内容聊天内容',
+            time: '下午6:15'
+          },
+          {
+            image: 'https://dn-mhke0kuv.qbox.me/6fe34414c019596859e1.jpg?imageView2/1/w/164/h/164/q/85/format/jpg/interlace/1',
+            name: '名字3',
+            content: '聊天内容聊天内容',
+            time: '下午6:15'
+          },
+          {
+            image: 'https://dn-mhke0kuv.qbox.me/6fe34414c019596859e1.jpg?imageView2/1/w/164/h/164/q/85/format/jpg/interlace/1',
+            name: '名字3',
+            content: '聊天内容聊天内容',
+            time: '下午6:15'
+          },
+          {
+            image: 'https://dn-mhke0kuv.qbox.me/6fe34414c019596859e1.jpg?imageView2/1/w/164/h/164/q/85/format/jpg/interlace/1',
+            name: '名字3',
+            content: '聊天内容聊天内容',
+            time: '下午6:15'
+          },
+        ]
+      };
     }
     onRightButton = () => {
       console.log('里面')
     }
     render() {
         return (
-          <View>
-            <Text>微信</Text>
-            <Text>微信</Text>
-            <Text>微信</Text>
-            <Text>微信</Text>
-            <Text>微信</Text>
+          <View style={styles.container}>
+            <ScrollView
+              keyboardDismissMode="on-drag"
+              >
+              {
+                (() => {
+                  return this.state.messages.map((message, index) => {
+                    return <List {...message} key={index}></List>
+                  })
+                })()
+              }
+            </ScrollView>
           </View>
         );
     }
 }
+class List extends Component {
+  constructor (props) {
+    super(props)
+  }
+  render () {
+    return (
+      <TouchableOpacity style={styles.list}>
+        <View style={styles.listContainer}>
+          <Image style={styles.listImage} source={{uri: this.props.image}} />
+          <View style={styles.listContent}>
+            <Text style={styles.listContentTitle}>{this.props.name}</Text>
+            <Text
+              numberOfLines={1}
+              style={styles.listContentText}>
+              {this.props.content}
+            </Text>
+            <Text style={styles.listContentTime}>{this.props.time}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingBottom: 50,
+  },
+  list: {
+    height: 60,
+    paddingLeft: 10
+  },
+  listContainer: {
+    height: 60,
+    borderBottomWidth: 1,
+    borderBottomColor: '#eee',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  listImage: {
+    width: 45,
+    height: 45,
+    backgroundColor: 'red',
+    borderRadius: 5,
+  },
+  listContent: {
+    flex: 1,
+    height: 45,
+    paddingLeft: 10,
+  },
+  listContentTitle: {
+    fontSize: 16,
+    height: 25,
+    lineHeight: 25,
+    fontWeight: 'bold',
+  },
+  listContentText: {
+    height: 20,
+    lineHeight: 20,
+    color: '#aaa',
+  },
+  listContentTime: {
+    position: 'absolute',
+    right: 10,
+    top: 5,
+    fontSize: 12,
+    color: '#bbb'
+  }
+})
